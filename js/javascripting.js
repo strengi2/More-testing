7eb1cdd0b29a48086301858cceb071c3765596f8
strengi2 <oskari.streng@gmail.com>
2023-03-09 13:42:09 +0200

    ,.

--- js/javascripting.js
+++ js/javascripting.js
@@ -1,1 +1,14 @@
-alert("moi");
+var cat = "kissa";
+var age = "9";
+var name = "mirri";
+var dog = "koira";
+
+alert(cat + " jonka ystävä on " + dog + " nimi on " + name + " ikä on " + age) 
+
+console.log(cat)
+console.error("HEI " + cat + " on elossa")
+console.warn("oletko sinä " + dog )
+
+var test = document.getElementById("test");
+
+test.innerHTML = "<u>HELLO</u>;"
